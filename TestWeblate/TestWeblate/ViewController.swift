15d4eb8de593b2d4c18698d928ff863d37a18f3d
ankit <ankit.sachan16@gmail.com>
2020-01-18 08:59:00 +0530

    Initial commit

---
+++ TestWeblate/TestWeblate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWeblate
+//
+//  Created by ankit on 18/01/20.
+//  Copyright © 2020 humanLearning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
